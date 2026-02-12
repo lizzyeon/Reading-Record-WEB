8391320c18b394986f1b883a89dfbcc6d8223682
JEEYEON LEE <joeuni94@gmail.com>
2026-02-12 17:58:01 +0900

    26-02-12

---
+++ main.py
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Google Icons -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
+
+    <title>독서기록장</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light position-relative px-0">
+<div class="container-fluid d-flex align-items-center">
+  <!-- 왼쪽 : 로고 -->
+  <a class="navbar-brand" href="#">
+      <img style="width:50px" src="images/logo.jpg"></a>
+
+  <!-- 가운데 : 검색창 -->
+          <div class="mx-auto d-flex" style="width:300px;">
+          <input class="form-control me-2" style="" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">search</button>
+          </div>
+
+  <!-- 오른쪽 : 아이콘 -->
+          <div class="d-flex gap-3 ms-auto">
+          <span class="material-symbols-outlined">home</span>
+          <span class="material-symbols-outlined">send</span>
+          <span class="material-symbols-outlined">add_box</span>
+          <span class="material-symbols-outlined">explore</span>
+          <span class="material-symbols-outlined">mood_heart</span>
+          </div>
+</div>
+</nav>
